e06e42a3c6a8e6807d4d2e402749cead0e7b3106
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-22 08:34:20 +0000

    added prettier

--- App.js
+++ App.js
@@ -1,25 +1,18 @@
 import "react-native-gesture-handler";
 import * as React from "react";
-import {  View, Text } from "react-native";
-import {  Provider as PaperProvider } from "react-native-paper";
-
+import { View, Text } from "react-native";
+import { Provider as PaperProvider } from "react-native-paper";
 
 function Main() {
   return (
-
-      <PaperProvider >
-     
-        <View>
-    <Text>
-      Hello
-    </Text>
-  </View>
-    
-      </PaperProvider>
- 
+    <PaperProvider>
+      <View>
+        <Text>Hello</Text>
+      </View>
+    </PaperProvider>
   );
 }
 
 export default function App() {
-  return <Main />
+  return <Main />;
 }
