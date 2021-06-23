fc11750bd4a21563fa9e58198c64d9b3ea94ff8a
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-23 14:22:14 +0000

    added BottomNavigation

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
 import "react-native-gesture-handler";
 import * as React from "react";
 import { Provider as PaperProvider } from "react-native-paper";
-import List from "./List";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function CartScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 function Main() {
   return (
     <PaperProvider>
-      <List />
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Cart" component={CartScreen} />
+          <Tab.Screen name="Profile" component={ProfileScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
