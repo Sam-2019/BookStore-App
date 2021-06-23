029237558004961d36387ff0a2aefbdc54a4aef9
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-23 15:13:27 +0000

    moved to own folder

--- Screens/Home/Home.js
+++ Screens/Home/Home.js
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { Text, View } from "react-native";
+import Product from "../../Components/product";
 
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Settings!</Text>
+      <Product />
     </View>
   );
 }
