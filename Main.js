a6a0cad47c9d5e18e6026022191d137171c614bb
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-24 12:02:11 +0000

    updayed Home Tab

--- Main.js
+++ Main.js
@@ -4,7 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 
-import HomeScreen from "./Screens/Home/Home";
+import HomeStack from "./Screens/Home/Home";
 import CartScreen from "./Screens/Cart/Cart";
 import ProfileScreen from "./Screens/User/User";
 
@@ -34,7 +34,7 @@ export default function Main() {
             inactiveTintColor: "gray",
           }}
         >
-          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Home" component={HomeStack} />
           <Tab.Screen name="Cart" component={CartScreen} />
           <Tab.Screen name="Profile" component={ProfileScreen} />
         </Tab.Navigator>
