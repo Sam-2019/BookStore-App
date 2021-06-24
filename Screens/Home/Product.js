2fb7ecc7824aab3762b52ef0ea6db55226c94622
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-24 12:46:36 +0000

    updated Product.js

--- Screens/Home/Product.js
+++ Screens/Home/Product.js
@@ -1,5 +1,13 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { ScrollView, View, Text } from "react-native";
+import {
+  Card,
+  ActivityIndicator,
+  Colors,
+  Button,
+  Title,
+  Paragraph,
+} from "react-native-paper";
 import { useRoute } from "@react-navigation/native";
 import { useQuery } from "@apollo/client";
 import { GET_PRODUCT } from "../../graphql functions";
@@ -11,9 +19,50 @@ export default function ProductScreen() {
     variables: { sku: route.params.sku },
   });
 
+  console.log(data);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <ActivityIndicator animating={true} color={Colors.red800} />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Error</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
-    </View>
+    <ScrollView>
+      <Card>
+        <Card.Cover source={{ uri: data.product.imageURL }} />
+        <Card.Title title={data.product.name} subtitle={data.product.author} />
+        <Card.Content>
+          <Paragraph>{data.product.detail}</Paragraph>
+        </Card.Content>
+      </Card>
+
+      <View>
+        <Button
+          mode="contained"
+          uppercase={true}
+          onPress={() => console.log("Pressed")}
+        >
+          buy now
+        </Button>
+        <Button
+          mode="outlined "
+          uppercase={true}
+          onPress={() => console.log("Pressed")}
+        >
+          add to cart
+        </Button>
+      </View>
+    </ScrollView>
   );
 }
