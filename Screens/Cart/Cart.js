a656a5ea0e95eaae59c46712ee87ccc5b8874d30
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-25 18:33:34 +0000

    added Cart

--- Screens/Cart/Cart.js
+++ Screens/Cart/Cart.js
@@ -1,10 +1,26 @@
 import * as React from "react";
-import { Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function CartScreen() {
+import CartListScreen from "./cartList";
+import Product from "../Home/Product"
+
+const Stack = createStackNavigator();
+
+export default function CartStack() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Settings!</Text>
-    </View>
+    <Stack.Navigator initialRouteName="Cart">
+      <Stack.Screen name="Cart" component={CartListScreen} />
+      <Stack.Screen
+        name="Product"
+        component={Product}
+        options={({ route }) => ({
+          title: route.params.sku,
+          headerTintColor: "black",
+          headerTitleStyle: {
+            textTransform: "capitalize",
+          },
+        })}
+      />
+    </Stack.Navigator>
   );
 }
