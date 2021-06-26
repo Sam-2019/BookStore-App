640d751876c993f156042ea7c3da56ca807b6499
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-26 19:19:51 +0000

    updated Account

--- Screens/Account/Account.js
+++ Screens/Account/Account.js
@@ -1,10 +1,50 @@
 import * as React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, View, Text, Pressable } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Account() {
+  const navigation = useNavigation();
+
+  const order = () => {
+    navigation.navigate("Order");
+  };
+
+  const wishlist = () => {
+    navigation.navigate("Wishlist");
+  };
+  
   return (
-    <View>
-      <Text>Welcome</Text>
+    <View style={styles.container}>
+      <View style={styles.item}>
+        <Pressable onPress={order}>
+          <Text style={styles.text}>Orders</Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.item}>
+        <Pressable onPress={wishlist}>
+          <Text style={styles.text}>Wishlist</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 8,
+  },
+  item: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "white",
+    marginBottom: 8,
+    marginRight: 10,
+    marginLeft: 10,
+    borderRadius: 10,
+    padding: 10,
+  },
+  text: {
+    fontSize: 20,
+  },
+});
